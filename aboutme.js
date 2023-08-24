56f8f9ef314f3aef38586908484a7f7ba4bd9a70
Lucifer Yadav <zemerikY@gmail.com>
2023-08-24 17:27:37 +1000

    aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,25 @@
+class AboutMe {
+  constructor() {
+    this.name = "Hemang";
+    this.username = "Zemerik";
+    this.discord = ".zemerik";
+    this.gender = "Male";
+  }
+  whoAmI() {
+    return ["cricket enthusiast", "coder/programmer/developer", "high school student"];
+  }
+  whatIDo() {
+    return ["Web/App Development", "Customised Bots", "Games"];
+  }
+  languages() {
+    return ["JavaScript", "HTML", "Python", "Java", "Scrath"];
+  }
+  tech() {
+    return ["Node.js", "npm", "discord.js", "discord.py"];
+  }
+  hobies() {
+    return["chess", "maths", "swimmming", "cricket"];
+  interestingFacts() {
+    return ["self taught", "young"];
+  }
+} 
